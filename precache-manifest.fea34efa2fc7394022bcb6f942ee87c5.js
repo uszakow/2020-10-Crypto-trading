a75bb7aa55ad7a3fa01c069c73d1ecd02191688e
Paweł Uszakow <51965778+uszakow@users.noreply.github.com>
2020-10-22 14:16:24 +0200

    Updates

---
+++ precache-manifest.fea34efa2fc7394022bcb6f942ee87c5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d90c94908081e8d81fc0788b31290b",
+    "url": "/2020-10-Crypto-trading/index.html"
+  },
+  {
+    "revision": "19400a6bc68a51209974",
+    "url": "/2020-10-Crypto-trading/static/css/main.dafcbe76.chunk.css"
+  },
+  {
+    "revision": "1936400a0d0214d9a4c8",
+    "url": "/2020-10-Crypto-trading/static/js/2.0ab6c8e6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/2020-10-Crypto-trading/static/js/2.0ab6c8e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19400a6bc68a51209974",
+    "url": "/2020-10-Crypto-trading/static/js/main.a4c25679.chunk.js"
+  },
+  {
+    "revision": "e6ccb5a7de5cb007816d",
+    "url": "/2020-10-Crypto-trading/static/js/runtime-main.413514c0.js"
+  },
+  {
+    "revision": "dbe40f9ab0ca98303966e5c30d46fc10",
+    "url": "/2020-10-Crypto-trading/static/media/Muxia.dbe40f9a.mp4"
+  },
+  {
+    "revision": "cbd2ffecfe58578786e4bc6e9afc246c",
+    "url": "/2020-10-Crypto-trading/static/media/background.cbd2ffec.png"
+  },
+  {
+    "revision": "bd1143139e3d0c761761702d94416610",
+    "url": "/2020-10-Crypto-trading/static/media/banks.bd114313.png"
+  },
+  {
+    "revision": "0d72715ea3ef0d66a93a00ceb6065bb9",
+    "url": "/2020-10-Crypto-trading/static/media/fb.0d72715e.svg"
+  },
+  {
+    "revision": "332e3393cf9f94bacc994b89baaa8b19",
+    "url": "/2020-10-Crypto-trading/static/media/fb.332e3393.svg"
+  },
+  {
+    "revision": "deddbf85dae626fc9647f31b6621c824",
+    "url": "/2020-10-Crypto-trading/static/media/linkedin.deddbf85.svg"
+  },
+  {
+    "revision": "3be83869c1b9491b043a9bda073df3f3",
+    "url": "/2020-10-Crypto-trading/static/media/martin.3be83869.png"
+  },
+  {
+    "revision": "272fb3b65aee7e8474c04d40768260ea",
+    "url": "/2020-10-Crypto-trading/static/media/morrison.272fb3b6.png"
+  },
+  {
+    "revision": "fb43339e8eed29980b4be0e840629da4",
+    "url": "/2020-10-Crypto-trading/static/media/parsons.fb43339e.png"
+  },
+  {
+    "revision": "ed7defa938ece7aced7e7d0271c702e8",
+    "url": "/2020-10-Crypto-trading/static/media/twitter.ed7defa9.svg"
+  }
+]);
